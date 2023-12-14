0222a0fceab2e3877733c3aec2a6fb04ddf571f9
stefanjanko <stefanjanko.dev@gmail.com>
2023-12-14 19:40:12 +0100

    Initial display for login and token rendering.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import RegistrationForm from './RegistrationForm';
+import LoginForm from './LoginForm';
 
 function App() {
   return (
     <div className="App">
       <h1>Your App Title</h1>
       <RegistrationForm />
+      <LoginForm />
     </div>
   );
 }

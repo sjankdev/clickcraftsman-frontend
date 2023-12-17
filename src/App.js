3ea4ac0d8b9759db229e65b5fc70d6c4fc48afdd
stefanjanko <stefanjanko.dev@gmail.com>
2023-12-17 19:05:31 +0100

    Login form with validation and CSS

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import RegistrationForm from './RegistrationForm';
-import LoginForm from './LoginForm';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import RegistrationForm from './components/security/RegistrationForm';
+import LoginForm from './components/security/LoginForm';
+import ClientHomepage from './components/homepages/ClientHomepage';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Your App Title</h1>
-      <RegistrationForm />
-      <LoginForm />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/register" element={<RegistrationForm />} />
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/clientHomepage" element={<ClientHomepage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
